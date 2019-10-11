8cc28568dd8b77de6c5e6822538064a9f512e707
Erik MK <inboxtoerik@gmail.com>
2019-10-11 14:48:48 +0200

    WIP: bulk generation of maze to product data for plotting

---
+++ gen_mazes_for_plotting.py
@@ -0,0 +1,27 @@
+from model import MazeGenerator, MazeSerializer
+from model import Resolver
+
+
+def generate_mazes(size: int, amount: int) -> list:
+    mazes = []
+    for i in range(amount):
+        mg = MazeGenerator()
+        maze = mg.generate(size, size)
+        mazes.append(maze)
+
+    return mazes
+
+
+def solve_mazes(mazes: list, times: int) -> list:
+    for maze in mazes:
+        resolver = Resolver(maze)
+        resolver.solve(times)
+
+    return mazes
+
+
+def save_mazes(mazes: list):
+    for maze in mazes:
+        maze_export = MazeSerializer(maze, datatype="json")
+        maze_export.save()
+
