b580dac05fd13b3c75fcb7beed17a21a713e19a2
Erik MK <inboxtoerik@gmail.com>
2019-10-14 20:58:48 +0200

    independent utility methods to check files in folder

--- get_files_in_dir.py
+++ get_files_in_dir.py
@@ -10,11 +10,28 @@ def get_files_in_dir(fileformat: str, recursive: bool) -> list:
     return files
 
 
-def new_file_name(files):
-    numbers = re.findall(r"\d+", ", ".join(files))
-    highest_num = max(int(num) for num in numbers)
-    new_file_name = "maze{}.json".format(highest_num + 1)
-    return new_file_name
+def new_file_num(path: str) -> int:
+    path = path
+    # returns a list of files in mazes folder
+    files = [f for f in glob.glob(path + "*.*", recursive=False)]
+    # lambda function splits the files string by dot and slash returning just the filename
+    filenames = list(map(lambda x: x.split(".")[0].split("/")[1], files))
+    # lambda function returns just the digits after 'maze' in the filename
+    numbers = list(
+        filter(
+            lambda x: x.isdigit(),
+            map(lambda x: x.split("maze")[1].split("_")[0], filenames),
+        )
+    )
+
+    if numbers:
+        # find the highest number in the list
+        highest_num = max(int(num) for num in numbers)
+    else:
+        highest_num = 0
+    new_file_num = highest_num + 1
+
+    return new_file_num
 
 
 files = get_files_in_dir("*", recursive=False)
@@ -24,4 +41,4 @@ for idx, file in enumerate(files):
     print("{}.\t{}".format(idx + 1, file))
 
 
-print("new filename: ", new_file_name(files))
+print("new filename: ", new_file_name())
