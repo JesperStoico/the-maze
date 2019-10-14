546e8fb8d539ae4c85efb786cb0f32595abaa085
Erik MK <inboxtoerik@gmail.com>
2019-10-14 23:27:25 +0200

    fixed bug in resolver when adding stats to maze object

--- resolver.py
+++ resolver.py
@@ -1,13 +1,14 @@
 import copy
 from time import process_time
 
+
 def dfs(maze, route, steps, x=1, y=1, first_run=0):
     """Returns [route, total_steps]"""
     if first_run == 1:
         route = []
         steps = []
         solution = {}
-    
+
     steps.append([x, y])
     #  Check if we have reached the end
     if maze[x][y] == "2":
@@ -20,7 +21,7 @@ def dfs(maze, route, steps, x=1, y=1, first_run=0):
         and maze[x][y + 1] == "2"
     ):
         maze[x][y] = "3"
-        route.append([x, y])        
+        route.append([x, y])
         dfs(maze, route=route, steps=steps, x=x, y=y + 1)
     #  Check down
     elif (
@@ -33,22 +34,12 @@ def dfs(maze, route, steps, x=1, y=1, first_run=0):
         route.append([x, y])
         dfs(maze, route=route, steps=steps, x=x + 1, y=y)
     #  Check left
-    elif (
-        y > 0
-        and maze[x][y - 1] == "0"
-        or y > 0
-        and maze[x][y - 1] == "2"
-    ):
+    elif y > 0 and maze[x][y - 1] == "0" or y > 0 and maze[x][y - 1] == "2":
         maze[x][y] = "3"
         route.append([x, y])
         dfs(maze, route=route, steps=steps, x=x, y=y - 1)
     #  Check up
-    elif (
-        x > 0
-        and maze[x - 1][y] == "0"
-        or x > 0
-        and maze[x - 1][y] == "2"
-    ):
+    elif x > 0 and maze[x - 1][y] == "0" or x > 0 and maze[x - 1][y] == "2":
         maze[x][y] = "3"
         route.append([x, y])
         dfs(maze, route=route, steps=steps, x=x - 1, y=y)
@@ -56,9 +47,10 @@ def dfs(maze, route, steps, x=1, y=1, first_run=0):
         maze[x][y] = "3"
         del route[-1]
         dfs(maze, route=route, steps=steps, x=route[-1][0], y=route[-1][1])
-    
-    solution = {'route': route, 'steps': steps}        
+
+    solution = {"route": route, "steps": steps}
     return solution
+
 
 def resolve_maze(maze_obj, amount=1, solver=dfs):
     """
@@ -69,12 +61,12 @@ def resolve_maze(maze_obj, amount=1, solver=dfs):
     """
     maze = maze_obj
     maze_copy = copy.deepcopy(maze)
-    
+
     while amount > 0:
         start_time = process_time()
-        if solver == 'dfs':
-            solution = dfs(maze.maze, route=[], steps=[], first_run=1)            
-        time = process_time() - start_time        
-        maze.Stats.add_solution(solver, solution['route'], solution['steps'], time)
-        maze = copy.deepcopy(maze_copy)
+        if solver == "dfs":
+            solution = dfs(maze_copy.maze, route=[], steps=[], first_run=1)
+        time = process_time() - start_time
+        maze.Stats.add_solution(solver, solution["route"], solution["steps"], time)
+        maze_copy = copy.deepcopy(maze)
         amount -= 1
