e687b0c50d8fcc277a907a455650192c707dd2d8
Erik MK <inboxtoerik@gmail.com>
2019-10-24 08:54:42 +0200

    generated mazes for plotting

--- gen_mazes_for_plotting.py
+++ gen_mazes_for_plotting.py
@@ -20,7 +20,7 @@ def generate_mazes(size: int, amount: int) -> list:
     return mazes
 
 
-def solve_mazes(mazes: list, times: int) -> list:
+def solve_mazes(mazes: list, times: int, solver: str) -> list:
     for idx, maze in enumerate(mazes):
         print(
             "Solving Maze {} of size {}x{} - {} times..".format(
@@ -28,7 +28,7 @@ def solve_mazes(mazes: list, times: int) -> list:
             ),
             end=" ",
         )
-        resolve_maze(maze, times, solver="dfs")
+        resolve_maze(maze, times, solver=solver)
         print("Done!")
 
     return mazes
@@ -51,10 +51,15 @@ print("#" * 80)
 print("Bulk generating, solving and saving mazes for plotting".center(80))
 print("#" * 80)
 
+script_start = process_time()
 for i in range(5, 31, 5):
     start_time = process_time()
     mazes = generate_mazes(i, 10)
-    mazes = solve_mazes(mazes, 10)
+    mazes = solve_mazes(mazes, 10, 'dfs')
+    mazes = solve_mazes(mazes, 10, 'astar')
     save_mazes(mazes)
     proc_time = round(Decimal(process_time()) - Decimal(start_time), 2)
     print("Processing time: {} seconds".format(proc_time), end="\n\n")
+
+script_time = round(Decimal(process_time()) - Decimal(script_start), 2)
+print("Processing time: {} seconds".format(script_time), end="\n\n")
