9cce2176fdc15b9310079417646362ad96a5da9f
NoiseCow <jesper@stoico.dk>
2019-10-24 21:50:39 +0200

    Return data for the different solvers

--- plotting.py
+++ plotting.py
@@ -1,24 +1,42 @@
 from get_files_in_dir import get_files_in_dir as get_files
 import model
 
-def plot_data():
-    files = []
+
+def create_plot_data():
+    """Takes all files in our mazes data folder\n
+    Return : [[(width, height), 'solver', steps, avg. time, min time, max time]]"""
     files = get_files('*', recursive=False)
-    data_main = []
-    
+    data = []
     for file in files:
-        data = []        
         file = file.split("\\")[1]
-        file_data = model.MazeDeserializer().load(filename=file, datatype='json')        
+        file_data = model.MazeDeserializer().load(filename=file, datatype='json')
         stats_data = file_data.Stats.get_solutions()
         for stats in stats_data:
-            data_row = []
-            data_row.append(((file_data.width -1)//2, (file_data.height -1)//2))
-            data_row.append(stats['algo'])
-            data_row.append(len(stats['route']))
-            data_row.append(stats['time'])
-            data.append(data_row)
-        data_main.append(data)    
-    return data_main
+            algoritm = stats['algo']
+            if data == []:
+                data.append(extract_data(algoritm, stats, file_data))
+            if algoritm not in data[-1][1]:
+                data.append(extract_data(algoritm, stats, file_data))
+    return data
 
-print(plot_data())
+
+def extract_data(algo, stats, file_data):
+    """Returns data for the specific algoritm"""
+    data = []
+    if algo == 'dfs':
+        data.append(((file_data.width - 1) // 2, (file_data.height -1) // 2))
+        data.append(algo)
+        data.append(len(stats['route']))
+        data.append(file_data.Stats.time_avg(algo=algo))
+        data.append(file_data.Stats.time_min(algo=algo))
+        data.append(file_data.Stats.time_max(algo=algo))
+        return data
+    if algo == 'astar':
+        data.append(((file_data.width - 1) // 2, (file_data.height -1) // 2))
+        data.append(algo)
+        data.append(len(stats['route']))
+        data.append(file_data.Stats.time_avg(algo=algo))
+        data.append(file_data.Stats.time_min(algo=algo))
+        data.append(file_data.Stats.time_max(algo=algo))
+        return data
+    return 'Solver unknown'
